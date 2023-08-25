4587fd414cfaebb505a1aca66926e523942aeb7e
LearnToCode1138 <mdelgado1138@hotmail.com>
2023-08-24 22:03:03 -0700

    product update/delete

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -149,21 +149,36 @@ const createProduct = async (req, res, next) => {
             .returning('id')
             .then((prodId) => {
               console.log('prodId: ', prodId);
-              return trx('users')
-                .where('id', '=', req.userData.userId)
-                .update({
-                  products: db.raw('array_append(products, ?)', [prodId[0].id]),
-                })
-                .then(res.status(201).json({ product: newProd }))
-                .catch((err) => {
-                  return next(
-                    err ||
-                      new HttpError(
-                        'Unable to complete product registration',
-                        500
-                      )
-                  );
-                });
+              return (
+                trx('users')
+                  .where('id', '=', req.userData.userId)
+                  .update({
+                    products: db.raw('array_append(products, ?)', [
+                      prodId[0].id,
+                    ]),
+                  })
+                  // .then(async () => {
+                  //   if (req.file) {
+                  //     params = {
+                  //       Bucket: bucketName,
+                  //       Key: imageName,
+                  //       Body: req.file.buffer,
+                  //       ContentType: req.file.mimetype,
+                  //     };
+                  //     await s3.send(new PutObjectCommand(params));
+                  //   }
+                  // })
+                  .then(res.status(201).json({ product: newProd }))
+                  .catch((err) => {
+                    return next(
+                      err ||
+                        new HttpError(
+                          'Unable to complete product registration',
+                          500
+                        )
+                    );
+                  })
+              );
             })
             .then(trx.commit);
         })
@@ -174,67 +189,6 @@ const createProduct = async (req, res, next) => {
     .catch((err) => {
       return next(err || new HttpError('Unable to create product', 500));
     });
-
-  // return next(new HttpError('no errors', 500));
-
-  //////////////////////////////////////////////
-  // const createdProd = new Product({
-  //   name,
-  //   description,
-  //   gtin,
-  //   category,
-  //   type,
-  //   image: req.file ? imageName : null,
-  //   height,
-  //   width,
-  //   depth,
-  //   weight,
-  //   packagingType,
-  //   tempUnits,
-  //   minTemp,
-  //   maxTemp,
-  //   storageInstructions,
-  //   subscribers: [],
-  //   dateAdded: new Date().toISOString(),
-  //   datePublished: null,
-  //   dateInactive: null,
-  //   dateModified: null,
-  //   owner: req.userData.userId,
-  // });
-  // console.log('createdProd: ', createdProd);
-
-  // let params, user;
-
-  // try {
-  //   user = await User.findById(req.userData.userId);
-  // } catch (err) {
-  //   return next(new HttpError('Creating place failed, please try again', 500));
-  // }
-
-  // if (!user)
-  //   return next(new HttpError('Could not find user for provided id', 404));
-
-  // try {
-  //   const sess = await mongoose.startSession();
-  //   sess.startTransaction();
-  //   await createdProd.save({ session: sess }), user.products.push(createdProd);
-  //   await user.save({ session: sess });
-  //   await sess.commitTransaction();
-
-  //   if (req.file) {
-  //     params = {
-  //       Bucket: bucketName,
-  //       Key: imageName,
-  //       Body: req.file.buffer,
-  //       ContentType: req.file.mimetype,
-  //     };
-  //     await s3.send(new PutObjectCommand(params));
-  //   }
-  // } catch (err) {
-  //   return next(new HttpError('Creating place failed, please try again!', 500));
-  // }
-
-  // res.status(201).json({ product: createdProd.toObject({ getters: true }) });
 };
 
 const updateProduct = async (req, res, next) => {
@@ -245,167 +199,233 @@ const updateProduct = async (req, res, next) => {
     );
   }
 
-  const prodId = req.params.pid;
-  let product;
+  const requestedGtin = req.params.pid;
+  let existingProd;
 
-  try {
-    product = await Product.find({ gtin: prodId });
-  } catch (err) {
-    return next(
-      new HttpError('Something went wrong, could not update product', 500)
+  db.select('*')
+    .from('products')
+    .where('gtin', '=', requestedGtin)
+    .then((prod) => {
+      if (!prod.length) throw new HttpError('Product not found', 404);
+      if (req.userData.userId !== prod[0].owner)
+        throw new HttpError(
+          'You are not authorized to edit this product.',
+          401
+        );
+
+      existingProd = prod[0];
+
+      return db
+        .transaction((trx) => {
+          const {
+            name,
+            description,
+            category,
+            height,
+            width,
+            depth,
+            weight,
+            packagingType,
+            tempUnits,
+            minTemp,
+            maxTemp,
+            storageInstructions,
+            subscribers,
+            dateInactive,
+          } = req.body;
+
+          if (name) existingProd.name = name;
+          if (description) existingProd.description = description;
+          if (category) existingProd.category = category;
+          if (height) existingProd.height = height;
+          if (width) existingProd.width = width;
+          if (depth) existingProd.depth = depth;
+          if (weight) existingProd.weight = weight;
+          if (packagingType) existingProd.packagingtype = packagingType;
+          if (tempUnits) existingProd.tempunits = tempUnits;
+          if (minTemp) existingProd.mintemp = minTemp;
+          if (maxTemp) existingProd.maxtemp = maxTemp;
+          if (storageInstructions)
+            existingProd.storageinstructions = storageInstructions;
+
+          let fileType, oldImage, newImage;
+          if (req.file) {
+            fileType = req.file.mimetype.split('/')[1];
+            oldImage = product[0].image;
+            newImage = `${uuid}.${fileType}`;
+            existingProd.image = newImage;
+          }
+
+          console.log('subs: ', subscribers);
+          if (subscribers[0]) {
+            if (subscribers.length !== existingProd.subscribers.length) {
+              existingProd.subscribers = subscribers;
+              existingProd.datepublished = new Date().toISOString();
+            }
+          }
+
+          if (!subscribers[0] || !subscribers) {
+            existingProd.subscribers = [];
+            existingProd.datepublished = null;
+          }
+
+          if (dateInactive === new Date(0).toISOString()) {
+            existingProd.dateinactive = null;
+          } else {
+            existingProd.dateinactive = dateInactive;
+          }
+
+          existingProd.datemodified = new Date().toISOString();
+
+          console.log('existingProd: ', existingProd);
+
+          trx('products')
+            .where('id', '=', existingProd.id)
+            .update({
+              name: existingProd.name,
+              description: existingProd.description,
+              image: existingProd.image,
+              category: existingProd.category,
+              height: existingProd.height,
+              width: existingProd.width,
+              depth: existingProd.depth,
+              weight: existingProd.weight,
+              packagingtype: existingProd.packagingtype,
+              tempunits: existingProd.tempunits,
+              mintemp: existingProd.mintemp,
+              maxtemp: existingProd.maxtemp,
+              storageinstructions: existingProd.storageinstructions,
+              subscribers: existingProd.subscribers,
+              datemodified: existingProd.datemodified,
+              datepublished: existingProd.datepublished,
+              dateinactive: existingProd.dateinactive,
+            })
+            // .then(async () => {
+            //   if (req.file) {
+            //     //save new image
+            //     saveParams = {
+            //       Bucket: bucketName,
+            //       Key: newImage,
+            //       Body: req.file.buffer,
+            //       ContentType: req.file.mimetype,
+            //     };
+            //     await s3.send(new PutObjectCommand(saveParams));
+
+            //     //delete old image
+            //     deleteParams = {
+            //       Bucket: bucketName,
+            //       Key: oldImage,
+            //     };
+            //     await s3.send(new DeleteObjectCommand(deleteParams));
+            //   }
+            // })
+            .then(res.status(200).json({ updated: existingProd }))
+            .then(trx.commit)
+            .catch((err) => {
+              trx.rollback;
+              return next(
+                err || new HttpError('Could not complete product update', 500)
+              );
+            });
+        })
+        .catch((err) =>
+          next(err || new HttpError('Unable to update product', 500))
+        );
+    })
+    .catch((err) =>
+      next(err || new HttpError('Could not update product', 500))
     );
-  }
-
-  if (product[0].owner != req.userData.userId) {
-    const error = new HttpError(
-      'You are not authorized to edit this place.',
-      401
-    );
-    return next(error);
-  }
-
-  const {
-    name,
-    description,
-    category,
-    height,
-    width,
-    depth,
-    weight,
-    packagingType,
-    tempUnits,
-    minTemp,
-    maxTemp,
-    storageInstructions,
-    subscribers,
-    dateInactive,
-  } = req.body;
-
-  if (name) product[0].name = name;
-  if (description) product[0].description = description;
-  if (category) product[0].category = category;
-  if (height) product[0].height = height;
-  if (width) product[0].width = width;
-  if (depth) product[0].depth = depth;
-  if (weight) product[0].weight = weight;
-  if (packagingType) product[0].packagingType = packagingType;
-  if (tempUnits) product[0].tempUnits = tempUnits;
-  if (minTemp) product[0].minTemp = minTemp;
-  if (maxTemp) product[0].maxTemp = maxTemp;
-  if (storageInstructions) product[0].storageInstructions = storageInstructions;
-
-  let fileType, oldImage, newImage;
-
-  if (req.file) {
-    fileType = req.file.mimetype.split('/')[1];
-    oldImage = product[0].image;
-    newImage = `${uuid}.${fileType}`;
-    product[0].image = newImage;
-  }
-
-  if (subscribers[0]) {
-    const subArray = subscribers.split(',');
-    product[0].subscribers = [];
-    subArray.forEach((sub) => product[0].subscribers.push(+sub));
-    product[0].datePublished = new Date().toISOString();
-  }
-
-  if (!subscribers[0] || !subscribers) {
-    product[0].subscribers = [];
-    product[0].datePublished = null;
-  }
-
-  if (dateInactive === new Date(0).toISOString()) {
-    product[0].dateInactive = null;
-  } else {
-    product[0].dateInactive = dateInactive;
-  }
-
-  product[0].dateModified = new Date().toISOString();
-
-  try {
-    const sess = await mongoose.startSession();
-    sess.startTransaction();
-    await product[0].save();
-    await sess.commitTransaction();
-
-    if (req.file) {
-      //save new image
-      saveParams = {
-        Bucket: bucketName,
-        Key: newImage,
-        Body: req.file.buffer,
-        ContentType: req.file.mimetype,
-      };
-      await s3.send(new PutObjectCommand(saveParams));
-
-      //delete old image
-      deleteParams = {
-        Bucket: bucketName,
-        Key: oldImage,
-      };
-      await s3.send(new DeleteObjectCommand(deleteParams));
-    }
-  } catch (err) {
-    return next(
-      new HttpError('Something went wrong, could not update product', 500)
-    );
-  }
-
-  res.status(200).json({ product: product[0].toObject({ getters: true }) });
 };
 
 const deleteProduct = async (req, res, next) => {
-  const prodId = req.params.pid;
+  const requestedGtin = req.params.pid;
+  let deleteProd;
 
-  let deleteProd, deleteUser;
+  db.select('*')
+    .from('products')
+    .where('gtin', '=', requestedGtin)
+    .then((prod) => {
+      if (!prod.length) throw new HttpError('Product not found', 404);
+      if (req.userData.userId !== prod[0].owner)
+        throw new HttpError(
+          'You are not authorized to edit this product.',
+          401
+        );
 
-  try {
-    deleteProd = await Product.find({ gtin: prodId });
-    deleteUser = await User(deleteProd[0].owner);
-  } catch (err) {
-    return next(
-      new HttpError('Something went wrong, could not delete product', 500)
+      deleteProd = prod[0];
+
+      return db
+        .transaction((trx) => {
+          return trx('products')
+            .where('gtin', '=', requestedGtin)
+            .del()
+            .then(() => {
+              trx.commit;
+              res.status(200).json({
+                message: `Product ${requestedGtin} ${deleteProd.name} has been deleted`,
+              });
+            })
+            .catch((err) => {
+              trx.rollback;
+              return next(
+                err || new HttpError('Could not complete product deletion', 500)
+              );
+            });
+        })
+        .catch((err) =>
+          next(err || new HttpError('Unable to delete product', 500))
+        );
+    })
+    .catch((err) =>
+      next(err || new HttpError('Unable to delete product', 500))
     );
-  }
 
-  if (!deleteProd || deleteProd.length === 0)
-    return next(new HttpError('Could not find product for this id', 404));
+  // try {
+  //   deleteProd = await Product.find({ gtin: prodId });
+  //   deleteUser = await User(deleteProd[0].owner);
+  // } catch (err) {
+  //   return next(
+  //     new HttpError('Something went wrong, could not delete product', 500)
+  //   );
+  // }
 
-  if (deleteProd[0].owner !== req.userData.userId)
-    return next(
-      new HttpError('You are not authorized to delete this product.', 401)
-    );
+  // if (!deleteProd || deleteProd.length === 0)
+  //   return next(new HttpError('Could not find product for this id', 404));
 
-  const deleteProdImage = deleteProd[0].image;
+  // if (deleteProd[0].owner !== req.userData.userId)
+  //   return next(
+  //     new HttpError('You are not authorized to delete this product.', 401)
+  //   );
 
-  try {
-    const sess = await mongoose.startSession();
-    sess.startTransaction();
-    await deleteProd[0].remove({ session: sess });
-    deleteUser.products.pull(deleteProd[0].id);
-    await deleteUser.save({ session: sess });
-    await sess.commitTransaction();
+  // const deleteProdImage = deleteProd[0].image;
 
-    if (deleteProdImage) {
-      const params = {
-        Bucket: bucketName,
-        Key: deleteProdImage,
-      };
-      await s3.send(new DeleteObjectCommand(params));
-    }
-  } catch (err) {
-    return next(
-      new HttpError(
-        'Something went wrong, could not complete product delete',
-        500
-      )
-    );
-  }
+  // try {
+  //   const sess = await mongoose.startSession();
+  //   sess.startTransaction();
+  //   await deleteProd[0].remove({ session: sess });
+  //   deleteUser.products.pull(deleteProd[0].id);
+  //   await deleteUser.save({ session: sess });
+  //   await sess.commitTransaction();
 
-  res.status(200).json({
-    message: `Product ${prodId} ${deleteProd[0].name} has been deleted`,
-  });
+  //   if (deleteProdImage) {
+  //     const params = {
+  //       Bucket: bucketName,
+  //       Key: deleteProdImage,
+  //     };
+  //     await s3.send(new DeleteObjectCommand(params));
+  //   }
+  // } catch (err) {
+  //   return next(
+  //     new HttpError(
+  //       'Something went wrong, could not complete product delete',
+  //       500
+  //     )
+  //   );
+  // }
+
+  // res.status(200).json({
+  //   message: `Product ${prodId} ${deleteProd[0].name} has been deleted`,
+  // });
 };
 
 exports.getProductById = getProductById;
